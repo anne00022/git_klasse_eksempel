56d3c5e138d53e7ccfc12c8e2a7bcf26ea298be6
Anna <anne0002@stud.kea.dk>
2024-02-06 15:27:31 +0100

    produktlist, produkt

---
+++ liste.js
@@ -0,0 +1,37 @@
+//console.log("Test");
+
+//fetch
+
+fetch("https://kea-alt-del.dk/t7/api/products?limit=50")
+  .then((res) => res.json())
+  .then(showProducts);
+
+function showProducts(products) {
+  //loope
+  products.forEach(showProduct);
+}
+
+function showProduct(product) {
+  //clone, ændre, appende
+
+  const template = document.querySelector("#smallProductTemplate").content;
+  const copy = template.cloneNode(true);
+
+  copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  copy.querySelector("h3").textContent = product.productdisplayname;
+  copy.querySelector("h5").textContent = product.brandname;
+  copy.querySelector("h6").textContent = product.price + " DKK";
+  copy.querySelector("div.discounted").textContent = "-" + product.discount + "%";
+
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldOut");
+  }
+
+  copy.querySelector(".link_produkt").setAttribute("href", `produkt.html?id=${product.id}`);
+
+  //append
+  document.querySelector("main").appendChild(copy);
+
+  const parent = document.querySelector(".produktliste_grid");
+  parent.appendChild(copy);
+}
---
+++ produkt.js
@@ -0,0 +1,21 @@
+//console.log("singleview");
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+fetch("https://kea-alt-del.dk/t7/api/products/" + id)
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".produkt_grid img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  document.querySelector(".purchaseBox h1").textContent = product.productdisplayname;
+  document.querySelector(".purchaseBox .brand").textContent = product.brandname;
+  document.querySelector(".purchaseBox .category").textContent = product.subcategory;
+  document.querySelector(".purchaseBox .pris").textContent = product.price;
+  document.querySelector(".purchaseBox .id").textContent = product.id;
+  document.querySelector(".purchaseBox .gender").textContent = product.gender;
+  document.querySelector(".purchaseBox .season").textContent = product.season;
+  document.querySelector(".purchaseBox .usagetype").textContent = product.usagetype;
+}
